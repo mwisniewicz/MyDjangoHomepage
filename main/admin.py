73d8dd500b4943d2eb7a506c94fbad93e9baba48
Mateusz Wisniewicz <m.wisniewicz@gmail.com>
2017-12-04 21:17:57 +0100

    Created example Models in main app

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from main.models import Topic,Webpage,AccessRecord
 
-# Register your models here.
+admin.site.register(AccessRecord)
+admin.site.register(Webpage)
+admin.site.register(Topic)
