787581e5c8845a232986eefdbb800a2349f316c0
Mateusz Wisniewicz <m.wisniewicz@gmail.com>
2017-12-21 01:24:56 +0100

    Adde template tags in Blog Template pages

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from blog import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.index, name="index"),
     path('create/', views.form_article_view, name="form_article")
